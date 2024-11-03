bdd3a5fb6e5319c3cd6ac818ba37f5c4f406a437
Poni <a0938659639@gmail.com>
2024-11-03 13:57:19 +0800

    vue-practice ch12-ch19

---
+++ 19_單文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 組件的結構 -->
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+//組件交互相關的代碼(數據、方法等等)
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京昌平",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+/* 組件的樣式 */
+.demo {
+  background-color: orange;
+}
+</style>
